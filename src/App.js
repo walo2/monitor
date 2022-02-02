34c774a1876945cc3888904522b16f533f2e1a8a
walo2 <42192595+walo2@users.noreply.github.com>
2022-02-02 13:51:32 -0300

    GitHub Pages configuration

--- src/App.js
+++ src/App.js
@@ -7,10 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Oi, João.
+          Funciona
         </p>
         <a
           className="App-link"
