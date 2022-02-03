db613cb90f2c72a6dea2d706a19a3e5016fa0f27
walo2 <wilson.al.oliver@gmail.com>
2022-02-02 22:52:15 -0300

    Conexão com Firebase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import './firebase'
+import { getDatabase, ref, onValue } from "firebase/database";
+
+
+const db = getDatabase();
+const data = ref(db, 'porta_fechada');
+onValue(data, (snapshot) => {
+  console.log(snapshot.val());
+});
 
 function App() {
   return (
@@ -7,7 +16,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Funciona
+          Funciona me ajuda Deus aaaaaa
         </p>
         <a
           className="App-link"
